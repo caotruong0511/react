2717a3ae862f80542e7d0ea568390737e00dfe13
caotruong0511 <truongcmph15429@fpt.edu.vn>
2022-03-15 15:47:39 +0700

    fix

---
+++ my-app/dist/demo/cap2/app.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const testFunction_1 = require("function/testFunction");
+const a = 10;
+const b = 20;
+const myName = "Le Trong Dat";
+const myAge = 20;
+const myStatus = true;
+const myObj = { id: 1, name: "Dat", price: 20 };
+const arrNumber = [1, 2, 3, 4];
+const arrString = ["a", "b", "c"];
+const arrObj = [
+    { id: 1, name: "San pham A", price: 20 },
+    { id: 1, name: "San pham A", status: true }
+];
+(0, testFunction_1.sum)(a, b);
---
+++ my-app/dist/function/testFunction/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sum = void 0;
+const sum = (numA, numbB) => {
+    return numA + numbB;
+};
+exports.sum = sum;
